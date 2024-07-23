4de82a7ba59040864e4420d6e92d93ca7da8d93b
oksanadmitriieva <145439444+oksanadmitriieva@users.noreply.github.com>
2024-07-23 12:01:16 +0200

    up

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+   
+    if (contacts.length > 0) {
+     
+      contacts.pop();
+
+     
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+      console.log();
+    } else {
+      console.log();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
